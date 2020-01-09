cb6c3f5afe342b37b89ee12ee7ba3c2b2262c5f0
kingsley okpara <kingsconsult001@gmail.com>
2020-01-09 20:15:55 +0100

    I updated the README file I created a table using the markdown format

--- one_offs.py
+++ one_offs.py
@@ -4,6 +4,7 @@ def one_offs(*arg, **karg):
     
     print(f"The sum of the positional arguments is: {sum_of_arg}")
     print(f"The sum of the keywoard arguments is: {sum_of_karg}\n") 
+
     
 one_offs(1, 2, 3, 4, kk=8, yy=9)
 one_offs(jj=5)
